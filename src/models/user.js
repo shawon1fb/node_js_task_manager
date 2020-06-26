1ee2b4521097d6f45fdad1d48093627fef9f76b5
shawon1fb <shawon0187@gmail.com>
2020-06-26 23:37:39 +0600

    timestamps implemented

--- src/models/user.js
+++ src/models/user.js
@@ -5,51 +5,56 @@ const jwt = require('jsonwebtoken')
 const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a postive number')
-            }
-        }
-    },
-    tokens: [{
-        token: {
+        name: {
             type: String,
             required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email is invalid')
+                }
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+            trim: true,
+            validate(value) {
+                if (value.toLowerCase().includes('password')) {
+                    throw new Error('Password cannot contain "password"')
+                }
+            }
+        },
+        age: {
+            type: Number,
+            default: 0,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error('Age must be a postive number')
+                }
+            }
+        },
+        tokens: [{
+            token: {
+                type: String,
+                required: true,
 
-        }
-    }]
-})
+            }
+        }]
+    },
+    {
+        timestamps: true
+
+    }
+)
 
 
 userSchema.virtual('tasks', {
